87eef6c4caabc9516eca20d58d1f28cbb888cccf
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-04-29 04:38:04 +0900

    적/아군 파츠 조합

---
+++ Assets/Breadone/Script/EndingSceneManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Linq;
+using UnityEngine.UI;
+
+public class EndingSceneManager : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        gameObject.Child("KillCount").GetComponent<Text>().text = HeroInfo.Instance.TotalKills.ToString();
+        gameObject.Child("Name").GetComponent<Text>().text = HeroInfo.Instance.HeroName;
+
+        MakeHero();
+
+
+        ACubeGameJamRankSystem.Instance.postScore(HeroInfo.Instance.HeroName, HeroInfo.Instance.TotalKills, (r) =>
+        {
+            Debug.Log(r.message);
+        });
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void MakeHero()
+    {
+        //히어로 만들고
+
+
+
+        //Head.. 파츠변경
+
+
+
+
+        //이미지 파일만들고
+
+        //서버로 전송
+
+
+    }
+}
--- Assets/Breadone/Script/HeroInfo.cs
+++ Assets/Breadone/Script/HeroInfo.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class HeroInfo : Singleton<HeroInfo> {
 
+    public enum PARTS_INDEX
+    {
+        DUCK,
+        JACK,
+        LEGO,
+        NUTCRACKER,
+        ROBOT,
+        SNOWMAN,
+        TREE
+    }
+
     [HideInInspector]
     public string HeroName;
 
     [HideInInspector]
     public int WeaponIndex;
+
+    [HideInInspector]
+    public int TotalKills;
+
+    [HideInInspector]
+    public int parts_head;
+
+    [HideInInspector]
+    public int parts_body;
+
+    [HideInInspector]
+    public int parts_leg;
+
+    [HideInInspector]
+    public int parts_weapon;
+    
 
     protected override void Awake()
     {
