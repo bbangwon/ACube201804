bc6fb4e802e4798005cfd92d838c6d17c16c5730
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-04-28 12:20:12 +0900

    무기 이미지 추가 사운드 플레이어 스크립트 작업중

---
+++ Assets/Breadone/Script/SoundPlayable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundPlayable : MonoBehaviour {
+
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Play(string clipName)
+    {
+        if (audioSource.isPlaying)
+            audioSource.Stop();
+
+        audioSource.Play();       
+    }
+
+    public void Stop()
+    {
+        audioSource.Stop();
+    }
+
+
+	
+}
