dbf437d74b53af88be904ee42a3e90dd08f0ae81
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-04-29 00:07:16 +0900

    처음 시작할때 무기 변경

---
+++ Assets/Breadone/Script/HeroWeaponChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Linq;
+using System.Linq;
+
+public class HeroWeaponChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var weapons = gameObject.Descendants().Where(_ => _.name == "Weapon").Children()
+            .Select((itm, idx) =>  new { Item = itm, Index = idx });
+
+        var myWeapon = weapons.FirstOrDefault(_ => _.Index == HeroInfo.Instance.WeaponIndex).Item;
+
+        foreach (var w in weapons)
+            w.Item.SetActive(false);
+
+        myWeapon.SetActive(true);
+    }
+}
