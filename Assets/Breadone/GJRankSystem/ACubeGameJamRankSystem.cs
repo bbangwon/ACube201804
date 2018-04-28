604cd4d4f2f4b697af8b2ee308bfefd0c65ad193
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-04-28 11:32:14 +0900

    랭킹서버에 아바타 보내기 기능 추가

---
+++ Assets/Breadone/GJRankSystem/ACubeGameJamRankSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+
+public class ACubeGameJamRankSystem : GJRankSystem {
+
+    const string command_postAvatar = "postAvatar";
+
+    protected override void Awake()
+    {
+        base.Awake();
+        phpFile = "GameJamRank/ACubeGameJamRank.php";
+    }
+
+    //아바타 올릴때 호출
+    public void postAvatar(string nickname, int head_parts, int body_parts, int leg_parts, int weapon_parts,
+        System.Action<resultMessage> result)
+    {
+        JObject reqContent = new JObject();
+        reqContent.Add("nickname", nickname);
+        reqContent.Add("gameid", 1);
+        reqContent.Add("headparts", head_parts);
+        reqContent.Add("bodyparts", body_parts);
+        reqContent.Add("legparts", leg_parts);
+        reqContent.Add("weaponparts", weapon_parts);
+
+        StartCoroutine(PostData(GetRequestJson(command_postAvatar, reqContent), o => {
+            if (result != null)
+                result(MakeResult(o));
+        }));
+    }
+}
---
+++ Assets/Breadone/Script/postAvatarTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class postAvatarTest : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        ((ACubeGameJamRankSystem)ACubeGameJamRankSystem.Instance).postAvatar("빵원", 1, 1, 1, 1, (r) => {
+            Debug.Log(r.message);
+        });
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
