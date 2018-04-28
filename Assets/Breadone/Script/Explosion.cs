91bb06c02887e9d65ae6d5cdacbf90c91e771ce5
DESKTOP-45BGSFN\bbang <bbangwon.chung@gmail.com>
2018-04-29 01:22:26 +0900

    Explosion 추가

---
+++ Assets/Breadone/Script/Explosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DOVirtual.DelayedCall(1f, () =>
+        {
+            Destroy(gameObject);
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
